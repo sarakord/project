9af60240eb71dd5b86d343e445419d3e4bb8dca2
sara <sarait405@gmail.com>
2020-04-05 20:33:22 +0430

    404 & 503 error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<style>
+
+    *{
+        padding: 0;
+        margin: 0;
+    }
+
+    img{
+        width: 100%;
+        height: 100%;
+    }
+</style>
+<img src="{{asset('images/error/503.jpg')}}" alt="">
+</body>
+</html>
